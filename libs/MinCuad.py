799888fd98d3c3ee98b74dc3cccf9221ea6e636b
Vicente Yáñez <vicenteyanez@ug.uchile.cl>
2023-03-17 15:54:40 -0300

    Update MinCuad.py

--- libs/MinCuad.py
+++ libs/MinCuad.py
@@ -1,8 +1,6 @@
-__doc__ = """
-GF5013 - Metodos Inversos Aplicados a la Geofisica
-Primavera 2017
-Prof. Francisco Hernán Ortega Culaciati
-ortega.francisco@u.uchile.cl
+"""
+Francisco Hernán Ortega Culaciati
+ortega.francisco@uchile.cl
 Departamento de Geofísica - FCFM
 Universidad de Chile
 
@@ -16,11 +14,13 @@ import numpy as np
 ######
 def MinCuadSimple(G, d):
     """
-    Calcula la solución de G*m = d por el método de mínimos cuadrados simples. 
-    Devuelve un diccionario con m y Cm. 
-    Para el cálculo de la matriz de covarianza del modelo, se asume que los 
-    errores del ajuste (i.e., de la diferencia d-Gm) son i.i.d. con media nula 
-    y varianza unitaria.  
+    Estimates the unknown parameter vector m in G*m = d + eta using simple linear least
+    squares. Here eta is the error of the misfit (including data and model prediction
+    errors), assumed to be i.i.d. with zero mean and a variance with unitary value.
+    Returns a dictionary with keys 'm' and 'Cm' whose values are the estimated model m and
+    its a posteriori covariance matrix Cm, respectively.
+    The solution of this problem is a Maximum Likelihood solution if misfit errors are
+    i.i.d. with zero mean and a variance with unitary value. 
     """
     Ndata, Npar = G.shape
     Cm = np.linalg.lstsq( G.T.dot(G), np.eye(Npar), rcond = None )[0]
@@ -31,12 +31,14 @@ def MinCuadSimple(G, d):
 ######
 def MinCuadPesos(G, d, Wx):
     """
-    Calcula la solución de Wx*G*m = Wx*d por el método de mínimos cuadrados (con pesos). 
-    Devuelve un diccionario con m y Cm. 
-    Para el cálculo de la matriz de covarianza de los parámetros estimados del modelo, 
-    se asume que los errores del ajuste (i.e., de la diferencia d-Gm) siguen una 
-    distribución normal multivariada con media nula y matriz de covarianza Cx.
-    donde Cx = inv( Wx.T.dot(Wx) ) 
+    Estimates the unknown parameter vector m in Wx*G*m = Wx*d + eta using simple linear
+    least squares. eta is the error of the misfit (including data and model prediction
+    errors), assumed to be i.i.d. with zero mean and a variance with unitary value.
+    Returns a dictionary with keys 'm' and 'Cm' whose values are the estimated model m and
+    its a posteriori covariance matrix Cm, respectively.
+    The solution of this problem is a Maximum Likelihood solution if Wx is such that
+    Wx.T.dot(Wx) = inv(Cx) where Cx = Cd + Cp is the covariance matrix of the misfit, and
+    Cd, Cp are the covariance matrices of the data and model prediction, respectively.
     """
     return MinCuadSimple( Wx.dot(G), Wx.dot(d) )
 
